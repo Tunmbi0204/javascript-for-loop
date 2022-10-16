a3c8730ecb4680cfda09b10f58b35a2e6a1bafd2
TUNMBI <adelowotumbi1@gmail.com>
2022-10-17 00:30:06 +0200

    pushing latest corrections

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
-output = document.getElementById('output');
-let scores = (60, 77, 38, 19, 59, 47, 23);
+let output = document.getElementById('output');
+let scores = [60, 77, 38, 19, 59, 47, 23];
+let text = "";
 
-for(let m = 0; m < scores.length; m++){
-    let punishment = scores[m] - 5
-    let manipulated = (punishment + scores[m]) / 2
+for (let m = 0; m < scores.length; m++){
+    let punishment = scores[m] - 5;
+    let manipulated = punishment + scores[m] / 2;
+    
 
 
-    console.log(scores)
+    console.log(punishment);
 
-if (punishment > 50){
-    output.innerHTML += punishment + "- you passed wonderfully, your previous score is " + scores[m] +" after manipulation <br>"
+ if (punishment > 50){
+  text += punishment + "- you passed wonderfully, your previous score is " + scores[m] + " after manipulation" + "<br>";
+     output.innerHTML = text;
 }
 
-else if ((punishment + scores[m]) / 2 > 50){
-output.innerHTML += manipulated + "- you escaped my punishment because of my manipulated marks,your previous score is " + scores[m] +" after manipulation <br>"
+ else if (manipulated > 50){
+text += manipulated + "- you escaped my punishment because of my manipulated score, your previous score is " + scores[m] + " after manipulation <br>";
+output.innerHTML = text;
 }
 
  else {
-    output.innerHTML += punishment + "- you failed badly,your previous score " + scores[m] +" after manipulation <br>"
+    text += punishment + "- you failed badly,your previous score " + scores[m] + " after manipulation <br> ";
+    output.innerHTML =  text;
 }
 
 }
